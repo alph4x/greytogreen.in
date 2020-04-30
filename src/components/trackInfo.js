af753b2efd2ba17b0e5e1744afa1a1157f127962
alph4x <danisharora099@gmail.com>
2020-05-01 00:34:31 +0530

    in-progress

--- src/components/trackInfo.js
+++ src/components/trackInfo.js
@@ -5,6 +5,9 @@ import Map from "google-map-react";
 import marker from "../assets/images/LOGO-single.png";
 import { Card } from "react-bootstrap";
 import "./trackInfo.css";
+import cardTitleImg from "../assets/images/tree2.png";
+import x from "../assets/images/tree4.png";
+import logoSingle from "../assets/images/LOGO-single.png";
 
 const AnyReactComponent = ({ text }) => (
   <div>
@@ -48,11 +51,46 @@ export default class TrackInfo extends React.Component {
               />
             </Map>
           </div>
-          <div className="trackingInfo">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+          <div className="trackingInfoContainer">
+            <Card id="cardContainer">
+              <Card.Img variant="top" src={cardTitleImg} id="cardTitleImg" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>User's Name</Card.Title>
+                <div className="row cardBodyIllustrations">
+                  <div className="col card-col">
+                    <div className="row">
+                      <img
+                        className="col card-col cardBodyIllustration"
+                        src={x}
+                        alt="x"
+                      ></img>
+                      <p className="col card-col" style={{ margin: "auto" }}>
+                        <span id="numTrees" className="is-caslon">
+                          37
+                        </span>
+                        <span className="is-century" id="saplingSpan">
+                          saplings
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col card-col">
+                    <div className="row">
+                      <img
+                        className="col card-col cardBodyIllustration"
+                        src={x}
+                        alt="x"
+                      ></img>
+                      <p className="col card-col" style={{ margin: "auto" }}>
+                        <span id="date" className="is-caslon">
+                          28.06.19
+                        </span>
+                        <br></br>
+                        <span id="time">17:28 IST</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
